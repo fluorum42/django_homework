1faa7c9eddaa1fca55de329e50ff85912a46e47b
Polina <theidolsinside@gmail.com>
2023-01-28 20:49:05 +0000

    homework03

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import FirstAuthor, FirstBook, Author, Book, Reader, User, Article, Comment, Like, Dislike
 
-# Register your models here.
+
+admin.site.register(FirstAuthor)
+admin.site.register(FirstBook)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Reader)
+admin.site.register(User)
+admin.site.register(Article)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Dislike)
